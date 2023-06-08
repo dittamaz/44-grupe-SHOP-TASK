3ea00dbcef95e30749c380cdbb88ebb38dcbebce
dittamaz <129725385+dittamaz@users.noreply.github.com>
2023-06-08 09:43:56 +0300

    restart

--- js/Prekes.js
+++ js/Prekes.js
@@ -9,24 +9,3 @@ class Prekes {
         this.kiekis = 0;
         this.prekeSandelyje = true;
 }
-
-
-
-    prekeYra () {
-        if (this.prekeSandelyje) {
-            return 'Prekiu sandelyje yra'
-        }
-
-        this.prekeSandelyje = true;
-        return 'Gali parduoti';
-    }
-
-    prekesNera () {
-        if (!this.prekeSandelyje) {
-            return 0;
-        }
-
-        this.prekeSandelyje = false;
-        return 'Papildyti prekiu';
-    }
-}
